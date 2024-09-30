478dc6628e1f34f1bdbbb7784384dd36a6b0a2d2
Redstoneur <alfa.aog36@gmail.com>
2024-09-30 15:33:24 +0200

    Update ai.py
    correctif pylint

--- AI/Class/ai.py
+++ AI/Class/ai.py
@@ -1,3 +1,8 @@
+"""
+Ce module contient la classe AI pour l'entraînement et la prédiction des résultats des enquêtes
+criminelles à San Francisco en utilisant divers modèles d'apprentissage automatique.
+"""
+
 ####################################################################################################
 ### Importation des modules nécessaires ############################################################
 ####################################################################################################
@@ -85,70 +90,83 @@ class AI:
         self.train_file_path = os.path.join(directory, train_file)
         self.test_file_path = os.path.join(directory, test_file)
 
-        # Lire les fichiers CSV dans des DataFrames pandas
+        self.load_data()
+        self.categorize_data()
+        self.encode_data()
+        self.sample_data()
+        self.train_models()
+
+    def load_data(self):
+        """
+        Load training and test data from CSV files.
+        """
         self.df_train = pd.read_csv(self.train_file_path)
         self.df_test = pd.read_csv(self.test_file_path)
 
         # Vérifier si l'un des DataFrames est vide
         if self.df_test is None or self.df_train is None:
-            raise Exception("L'un des DataFrames est vide")
+            raise ValueError("L'un des DataFrames est vide")
 
-        # Catégoriser 'Resolution' en catégories plus larges
+    def categorize_data(self):
+        """
+        Catégoriser 'Resolution' en catégories plus larges.
+        """
         self.df_train['Categorie'] = ''
         self.df_train.loc[self.df_train['Resolution'].isin([
             'ARREST, BOOKED', 'ARREST, CITED', 'JUVENILE CITED',
             'JUVENILE BOOKED', 'PROSECUTED FOR LESSER OFFENSE',
-             'PROSECUTED BY OUTSIDE AGENCY'
+            'PROSECUTED BY OUTSIDE AGENCY'
         ]), 'Categorie'] = self.prediction_mapping[0]
         self.df_train.loc[self.df_train['Resolution'].isin([
             'COMPLAINANT REFUSES TO PROSECUTE', 'DISTRICT ATTORNEY REFUSES TO PROSECUTE',
-            'NOT PROSECUTED','JUVENILE ADMONISHED', 'JUVENILE DIVERTED',
+            'NOT PROSECUTED', 'JUVENILE ADMONISHED', 'JUVENILE DIVERTED',
             'CLEARED-CONTACT JUVENILE FOR MORE INFO', 'PSYCHOPATHIC CASE',
             'EXCEPTIONAL CLEARANCE'
         ]), 'Categorie'] = self.prediction_mapping[1]
         self.df_train.loc[self.df_train['Resolution'].isin([
-                'LOCATED', 'UNFOUNDED'
+            'LOCATED', 'UNFOUNDED'
         ]), 'Categorie'] = self.prediction_mapping[2]
         self.df_train.loc[self.df_train['Resolution'].isin([
             'NONE'
         ]), 'Categorie'] = self.prediction_mapping[3]
 
-        # Créer des sous-ensembles des DataFrames pour l'entraînement et le test
+    def encode_data(self):
+        """
+        Encode categorical features.
+        """
         df_train_patch = self.df_train[
             ['Dates', 'Categorie', 'DayOfWeek', 'PdDistrict', 'Address', 'X', 'Y']
         ]
-        df_test_patch = self.df_test[
-            ['Dates', 'DayOfWeek', 'PdDistrict', 'Address', 'X', 'Y']
-        ]  # todo : vérifier si c'est correct
-
-        # Encoder les caractéristiques catégorielles
         categorical_features = [
             col for col in df_train_patch.columns if self.df_train[col].dtype == 'object'
         ]
         self.encoder = OrdinalEncoder(cols=categorical_features).fit(df_train_patch)
-        df_train_patch_2 = self.encoder.transform(df_train_patch)
+        self.df_train_patch_2 = self.encoder.transform(df_train_patch)
 
-        # Échantillonner les données pour équilibrer les classes
+    def sample_data(self):
+        """Sample data to balance classes."""
         nb = 25000
-        df_train_patch_sample = pd.concat([
-            df_train_patch_2[df_train_patch_2.Categorie == i].sample(
-                min(nb, len(df_train_patch_2[df_train_patch_2.Categorie == i])), replace=True)
+        self.df_train_patch_sample = pd.concat([
+            self.df_train_patch_2[self.df_train_patch_2.Categorie == i].sample(
+                min(nb, len(self.df_train_patch_2[self.df_train_patch_2.Categorie == i])),
+                replace=True)
             for i in range(4)
         ])
 
-        # Séparer les caractéristiques (X) et les étiquettes (y)
-        y = df_train_patch_sample.Categorie
-        x = df_train_patch_sample.drop(['Categorie'], axis=1)
+    def train_models(self):
+        """
+        Entraîne les modèles d'arbre de décision, de forêt aléatoire et de KNN.
+        """
+        y = self.df_train_patch_sample.Categorie
+        x = self.df_train_patch_sample.drop(['Categorie'], axis=1)
         x_train, x_test, y_train, y_test = train_test_split(
             x, y, test_size=0.33, random_state=42
         )
 
-        # Entraîner un arbre de décision
         self.clf = tree.DecisionTreeClassifier().fit(x_train, y_train)
         self.acctree = self.clf.score(x_test, y_test) * 100
         print(f'Précision de l\'arbre de décision: {self.acctree:.2f}%')
 
-        # Entraîner une forêt aléatoire
         # noinspection PyTypeChecker
         self.rf_classifier = RandomForestClassifier(
             n_estimators=100, random_state=42
@@ -156,7 +174,6 @@ class AI:
         self.accrf = accuracy_score(y_test, self.rf_classifier.predict(x_test)) * 100
         print(f'Précision de la forêt aléatoire: {self.accrf:.2f}%')
 
-        # Entraîner un K-Nearest Neighbors
         self.knn = KNeighborsClassifier(n_neighbors=2).fit(x_train, y_train)
         self.accknn = accuracy_score(y_test, self.knn.predict(x_test)) * 100
         print(f'Précision du KNN: {self.accknn:.2f}%')
@@ -168,12 +185,24 @@ class AI:
         :param d: Nouvelles données à prédire.
         :return: Prédiction de l'issue de l'enquête.
         """
-        # Ignorer les avertissements de type FutureWarning liés à la méthode fillna
         warnings.filterwarnings("ignore", category=FutureWarning,
                                 message=".*Downcasting object dtype arrays on .fillna.*")
 
-        # Préparer les nouvelles données sous forme de dictionnaire
-        new_data: dict = {
+        new_df_encoded = self.prepare_data(d)
+        predictions = self.make_predictions(new_df_encoded)
+        final_prediction = self.determine_final_prediction(predictions)
+
+        final_prediction_text = self.prediction_mapping.get(final_prediction, "Catégorie inconnue")
+        return final_prediction_text
+
+    def prepare_data(self, d: Data) -> pd.DataFrame:
+        """
+        Prépare les nouvelles données pour la prédiction.
+
+        :param d: Nouvelles données à prédire.
+        :return: DataFrame encodé des nouvelles données.
+        """
+        new_data = {
             'Dates': [d.Dates],
             'DayOfWeek': [d.DayOfWeek],
             'PdDistrict': [d.PdDistrict],
@@ -182,38 +211,45 @@ class AI:
             'Y': [d.Y],
             "Categorie": [None]
         }
-
-        # Convertir les nouvelles données en DataFrame pandas
         new_df = pd.DataFrame(new_data)
-        # Encoder les nouvelles données et supprimer la colonne 'Categorie'
         new_df_encoded = self.encoder.transform(new_df).drop(columns=['Categorie'])
+        return new_df_encoded
 
-        # Faire des prédictions avec les trois modèles
+    def make_predictions(self, new_df_encoded: pd.DataFrame) -> list:
+        """
+        Fait des prédictions avec les trois modèles.
+
+        :param new_df_encoded: DataFrame encodé des nouvelles données.
+        :return: Liste des prédictions des trois modèles.
+        """
         tree_prediction = self.clf.predict(new_df_encoded)
         rf_prediction = self.rf_classifier.predict(new_df_encoded)
         knn_prediction = self.knn.predict(new_df_encoded)
+        return [tuple(tree_prediction), tuple(rf_prediction), tuple(knn_prediction)]
 
-        # Compter les prédictions de chaque modèle
-        predictions = [tuple(tree_prediction), tuple(rf_prediction), tuple(knn_prediction)]
+    def determine_final_prediction(self, predictions: list) -> int:
+        """
+        Détermine la prédiction finale en fonction des prédictions des modèles.
+
+        :param predictions: Liste des prédictions des trois modèles.
+        :return: Prédiction finale.
+        """
         prediction_counts = Counter(predictions)
         most_common_prediction, count = prediction_counts.most_common(1)[0]
 
-        # Si les modèles ne sont pas d'accord, choisir le modèle le plus précis
         if count == 1:
+            model_predictions = {
+                'tree': predictions[0],
+                'rf': predictions[1],
+                'knn': predictions[2]
+            }
             accuracies = {'tree': self.acctree, 'rf': self.accrf, 'knn': self.accknn}
             most_accurate_model = max(accuracies, key=accuracies.get)
-            final_prediction = eval(f'{most_accurate_model}_prediction')
+            final_prediction = model_predictions[most_accurate_model]
         else:
             final_prediction = most_common_prediction
 
-        # Convertir la prédiction finale en entier
-        final_prediction = int(final_prediction[0])
-
-        # Obtenir le texte de la prédiction finale à partir du mappage
-        final_prediction_text = self.prediction_mapping.get(final_prediction, "Catégorie inconnue")
-
-        # Retourner le texte de la prédiction finale
-        return final_prediction_text
+        return int(final_prediction[0])
 
     def get_accuracy(self) -> dict:
         """
@@ -238,7 +274,11 @@ class AI:
 # Point d'entrée principal du script
 if __name__ == "__main__":
     # Crée une instance de la classe AI avec les chemins des fichiers d'entraînement et de test
+    print("Initialisation de l'IA...")
     ai = AI("./../DataSet", "train.csv", "test.csv")
+    print("IA initialisée avec succès!")
+
+    print()
 
     # Crée un objet Data avec des valeurs d'exemple
     data = Data(Dates="2015-05-13 23:53:00", DayOfWeek="Wednesday", PdDistrict="SOUTHERN",
